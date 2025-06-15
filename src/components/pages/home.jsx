6b680c302d596569c2cac609a2325f0c233fb171
mfatihz.std <mfatihz.std@gmail.com>
2025-06-15 09:06:54 +0000

    minor: data at Home changed to static data

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+//import { useEffect, useState } from 'react';
 import HomeTemplate from "../templates/home-template"
 import { navData, menuData, genreData, helpData } from "../../utils/app/home-utils"
 import { movieHero, movieGalleries } from "../../utils/data/home-page-data"
@@ -9,19 +9,21 @@ function Home() {
   const footer = { genreData:genreData, helpData:helpData }
   
   /*
-    Note: pada tahapa ini data masih statis, bisa saja langsung di export dari utils tanpa perlu useState dan useEffect
-    const hero = movieHero
-    const galleries = movieGalleries
+    Note: pada tahap ini data masih statis, bisa langsung di export dari utils tanpa perlu useState dan useEffect  
+    const [ hero, setHero ] = useState([]);
+    const [ galleries, setGalleries ] = useState([]);
   */
-  const [ hero, setHero ] = useState([]);
-  const [ galleries, setGalleries ] = useState([]);
+  const hero = movieHero
+  const galleries = movieGalleries
 
   const { toggleId: mylistToggleHandler, hasId: isInMyListHandler } = useLocalStorage('my-lists', new Set());
 
+  /* 
   useEffect(() => {
     setHero(movieHero);
     setGalleries(movieGalleries);
   }, [movieHero, movieGalleries]);
+   */
 
   return (
     <HomeTemplate
