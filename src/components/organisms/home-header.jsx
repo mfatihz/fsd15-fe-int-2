ac19a3ba42812fbd8d6387a480d240999c108de3
mfatihz <fatih@reksolindo.co.id>
2025-06-20 09:03:23 +0700

    minor

--- src/components/organisms/home-header.jsx
+++ src/components/organisms/home-header.jsx
@@ -1,22 +1,3 @@
-// import clsx from 'clsx'
-// import { Link } from "react-router"
-// import ChillLogo from "../atoms/chill-logo"
-// import NavigationalLinks from "../molecules/navigational-links"
-// import SettingMenu from "./setting-menu"
-
-// function HomeHeader({ navData, menuData, padding}) {
-//   const baseStyle = "w-full flex gap-4 sm:gap-8 items-center"
-//   return (
-//     <header className={clsx(baseStyle, padding)}
-//     >
-//       <ChillLogo />
-//       <NavigationalLinks links={ navData } />
-//       <SettingMenu links={ menuData } className="ml-auto"/>
-//     </header>
-//   )
-// }
-
-// export default HomeHeader
 import React, { forwardRef } from "react";
 import ChillLogo from "../atoms/chill-logo";
 import NavigationalLinks from "../molecules/navigational-links";
